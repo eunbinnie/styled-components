3d20ba8d49f918f8ccbdac870dfa75cbd5dd550a
eunbinnie <dmsqlsdl54@naver.com>
2023-07-27 23:28:53 +0900

    styled-component 설치

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import styled from 'styled-components'
+
+const Component = styled.main`
+  background-color: '#FA5858';
+`;
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
