bf91bbddb511064d3475b08b19a860ccc2c7f95b
eunbinnie <dmsqlsdl54@naver.com>
2023-07-30 20:24:03 +0900

    Link(페이지 이동)

---
+++ src/pages/posts/about.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Text = styled.h1`
+    font-size: 32px;
+    font-weight: bold;
+    text-align: center;
+`;
+
+export default function About() {
+    return (
+        <>
+            <Text>내가 해.냈.다.</Text>
+        </>
+    )
+}
